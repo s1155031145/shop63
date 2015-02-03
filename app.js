8060acf657df9fa1a7ad9a959a9cfc7522b90625
s1155015942 <s1155015942@cuhk.edu.hk>
2015-02-04 02:13:26 +0800

    abc

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require("http");
-var express = require('express');
-var app = express();
-var __dirname = '/C/Users/USER/Desktop/ierg4210/shop63/Web'
+//var express = require('express');
+//var app = express();
+//var __dirname = '/C/Users/USER/Desktop/ierg4210/shop63/Web'
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
@@ -9,8 +9,8 @@ http.createServer(function(request, response) {
   response.end();
 }).listen(process.env.PORT || 8888);
 
-app.use('/images', express.static(__dirname + '/images'));
-app.use('/lib', express.static(__dirname + '/lib'));
-app.use('/css', express.static(__dirname + '/css'));
+//app.use('/images', express.static(__dirname + '/images'));
+//app.use('/lib', express.static(__dirname + '/lib'));
+//app.use('/css', express.static(__dirname + '/css'));
 
-app.listen(process.env.PORT || 8888);
+//app.listen(process.env.PORT || 8888);
