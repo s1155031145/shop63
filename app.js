102f65174353b9da6b1ba44a4057824cdc281eac
s1155015942 <s1155015942@cuhk.edu.hk>
2015-02-04 02:41:40 +0800

    abc

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 var http = require("http");
-//var express = require('express');
-//var app = express();
-//var __dirname = '/C/Users/USER/Desktop/ierg4210/shop63/Web'
+var app = express();
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(process.env.PORT || 8888);
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html'],
+  index: true,
+  maxAge: '1d',
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now())
+  }
+};
 
-//app.use('/images', express.static(__dirname + '/images'));
-//app.use('/lib', express.static(__dirname + '/lib'));
-//app.use('/css', express.static(__dirname + '/css'));
 
-//app.listen(process.env.PORT || 8888);
+
+
+app.use(express.static('public', options));
+
+app.listen(process.env.PORT || 8888);
