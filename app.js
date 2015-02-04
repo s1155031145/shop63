65ed642f23f2487dca9489d1ea0fb6ef406cf415
s1155015942 <s1155015942@cuhk.edu.hk>
2015-02-04 16:54:29 +0800

    abc

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var http = require("http");
-http.createServer(function(request, response) {
-response.writeHead(200, {"Content-Type": "text/plain"});
-response.write("Hello World");
-response.end();
-}).listen(process.env.PORT || 8888);
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('public/'));
+
+app.listen(process.env.PORT || 8888);
