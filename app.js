ef333ab5b094665224b0584d09199a3b4162a77a
s1155015942 <s1155015942@cuhk.edu.hk>
2015-02-04 16:43:14 +0800

    abc

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-'use strict';
-
 var express = require('express');
 
 var app = express();
 
 app.use(express.static('public/'));
 
-app.listen(3000, function () {
+app.listen(8888, function () {
     console.log('express-handlebars example server listening on: 3000');
 });
