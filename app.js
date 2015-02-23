8d66804ba53f297ed36b94ebc02a692dd469ac6b
s1155015942 <s1155015942@cuhk.edu.hk>
2015-02-23 20:45:03 +0800

    phase3b

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
 exphbs = require('express3-handlebars'),
-backEndRouter = require('./routes/backend.js');
+backEndRouter = require('./routes/backend.js'),
+frontEndRouter = require('./routes/frontend.js');
 
 var app = express();
 
@@ -15,5 +16,7 @@ app.set('view engine', 'hbs');
 
 app.use(express.static('public/'));
 app.use('/admin', backEndRouter);
+
+app.use('/', frontEndRouter);
 
 app.listen(process.env.PORT || 8888);
