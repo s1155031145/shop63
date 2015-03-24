f306a95b0538ccbf38238f5673b83751ee88c08c
s1155015942 <s1155015942@cuhk.edu.hk>
2015-03-24 16:31:18 +0800

    4b

--- app.js
+++ app.js
@@ -23,6 +23,17 @@ app.use(function(req, res, next){
     next();
 });
 
+app.use('/admin', function(req, res, next) {
+var schema = req.headers['x-forwarded-proto'];
+if (schema === 'https') {
+// Already https; don't do anything special.
+next();
+}
+else {
+// Redirect to https.
+res.redirect('https://' + req.headers.host + req.url + '/admin');
+}
+});
 
 app.use('/admin', authAPIRouter);
 app.use('/admin', backEndRouter);
